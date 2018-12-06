ad23abe0a30d580b0d569bcc196b912c0cf0281d
Your Name <you@example.com>
2018-12-06 14:29:18 +0800

    first commit

---
+++ src/com/pccu/textfilecopy/TextFileCopy.java
@@ -0,0 +1,47 @@
+package com.pccu.textfilecopy;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TextFileCopy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FileReader reader = null;
+		FileWriter writer = null;
+		
+		try {
+			
+			reader = new FileReader("src/textfile/abstract1.txt");
+			writer = new FileWriter("src/textfile/clonedabstract1.txt");
+			
+			int flag = 0;
+			while((flag = reader.read()) != -1) {
+				writer.write(flag);
+			}
+		}catch (FileNotFoundException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if(writer != null) {
+				try {
+					writer.close();
+				}catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if(reader != null) {
+				try {
+					reader.close();
+				}catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
